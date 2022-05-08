46cbb900aafa891d50d68d77d5aaae2a2ef55f71
BamOnGithub <82470343+Bammmmmmmmmmmmm@users.noreply.github.com>
2022-05-07 19:50:15 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vs Bams source code lolololololol
+
+
+# [BUILT ON FOREVER ENGINE](https://github.com/Yoshubs/FunkinForever)
+An open-source, quality of life and performance driven [Friday Night Funkin'](https://www.newgrounds.com/portal/view/770371) mod engine!
+----------------------------------------------
+
+![image](https://media.discordapp.net/attachments/916918345255751690/930068118938730526/legacy.png)
---
+++ source/meta/state/TitleState.hx
@@ -0,0 +1,379 @@
+package meta.state;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.system.FlxSound;
+import flixel.system.ui.FlxSoundTray;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+import meta.MusicBeat.MusicBeatState;
+import meta.data.*;
+import meta.data.dependency.Discord;
+import meta.data.font.Alphabet;
+import meta.state.menus.*;
+import openfl.Assets;
+
+using StringTools;
+
+/**
+	I hate this state so much that I gave up after trying to rewrite it 3 times and just copy pasted the original code
+	with like minor edits so it actually runs in forever engine. I'll redo this later, I've said that like 12 times now
+
+	I genuinely fucking hate this code no offense ninjamuffin I just dont like it and I don't know why or how I should rewrite it
+
+
+
+	Quit bullying to ninja muffin
+**/
+class TitleState extends MusicBeatState
+{
+	static var initialized:Bool = false;
+
+	var blackScreen:FlxSprite;
+	var credGroup:FlxGroup;
+	var credTextShit:Alphabet;
+	var textGroup:FlxGroup;
+	var ngSpr:FlxSprite;
+
+	var curWacky:Array<String> = [];
+
+	var wackyImage:FlxSprite;
+
+	override public function create():Void
+	{
+		controls.setKeyboardScheme(None, false);
+		curWacky = FlxG.random.getObject(getIntroTextShit());
+		super.create();
+
+		startIntro();
+	}
+
+	var logoBl:FlxSprite;
+	var gfDance:FlxSprite;
+	var danceLeft:Bool = false;
+	var titleText:FlxSprite;
+
+	function startIntro()
+	{
+		if (!initialized)
+		{
+			///*
+			#if !html5
+			Discord.changePresence('TITLE SCREEN', 'Main Menu');
+			#end
+			
+			ForeverTools.resetMenuMusic(true);
+		}
+
+		persistentUpdate = true;
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		// bg.antialiasing = true;
+		// bg.setGraphicSize(Std.int(bg.width * 0.6));
+		// bg.updateHitbox();
+		add(bg);
+
+		logoBl = new FlxSprite(-150, -100);
+		logoBl.frames = Paths.getSparrowAtlas('menus/base/title/logoBumpin');
+		logoBl.antialiasing = true;
+		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
+		logoBl.animation.play('bump');
+		logoBl.updateHitbox();
+		// logoBl.screenCenter();
+		// logoBl.color = FlxColor.BLACK;
+
+		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.03);
+		gfDance.frames = Paths.getSparrowAtlas('menus/base/title/bam');
+		gfDance.animation.addByPrefix('idle', "bamTitle", 24);
+		gfDance.antialiasing = true;
+		add(gfDance);
+		add(logoBl);
+
+		titleText = new FlxSprite(100, FlxG.height * 0.8);
+		titleText.frames = Paths.getSparrowAtlas('menus/base/title/titleEnter');
+		titleText.animation.addByPrefix('idle', "Press Enter to Begin", 24);
+		titleText.animation.addByPrefix('press', "ENTER PRESSED", 24);
+		titleText.antialiasing = true;
+		titleText.animation.play('idle');
+		titleText.updateHitbox();
+		// titleText.screenCenter(X);
+		add(titleText);
+
+		// var logo:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menus/base/title/logo'));
+		// logo.screenCenter();
+		// logo.antialiasing = true;
+		// add(logo);
+
+		// FlxTween.tween(logoBl, {y: logoBl.y + 50}, 0.6, {ease: FlxEase.quadInOut, type: PINGPONG});
+		// FlxTween.tween(logo, {y: logoBl.y + 50}, 0.6, {ease: FlxEase.quadInOut, type: PINGPONG, startDelay: 0.1});
+
+		credGroup = new FlxGroup();
+		add(credGroup);
+		textGroup = new FlxGroup();
+
+		blackScreen = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		credGroup.add(blackScreen);
+
+		credTextShit = new Alphabet(0, 0, "ninjamuffin99\nPhantomArcade\nkawaisprite\nevilsk8er", true);
+		credTextShit.screenCenter();
+
+		// credTextShit.alignment = CENTER;
+
+		credTextShit.visible = false;
+
+		ngSpr = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('menus/base/title/newgrounds_logo'));
+		add(ngSpr);
+		ngSpr.visible = false;
+		ngSpr.setGraphicSize(Std.int(ngSpr.width * 0.8));
+		ngSpr.updateHitbox();
+		ngSpr.screenCenter(X);
+		ngSpr.antialiasing = true;
+
+		FlxTween.tween(credTextShit, {y: credTextShit.y + 20}, 2.9, {ease: FlxEase.quadInOut, type: PINGPONG});
+
+		if (initialized)
+			skipIntro();
+		else
+			initialized = true;
+
+		// credGroup.add(credTextShit);
+	}
+
+	function getIntroTextShit():Array<Array<String>>
+	{
+		var swagGoodArray:Array<Array<String>> = [];
+		if (Assets.exists(Paths.txt('introText')))
+		{
+			var fullText:String = Assets.getText(Paths.txt('introText'));
+			var firstArray:Array<String> = fullText.split('\n');
+
+			for (i in firstArray)
+				swagGoodArray.push(i.split('--'));
+		}
+
+		return swagGoodArray;
+	}
+
+	var transitioning:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music != null)
+			Conductor.songPosition = FlxG.sound.music.time;
+		// FlxG.watch.addQuick('amp', FlxG.sound.music.amplitude);
+
+		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER;
+
+		#if mobile
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+			{
+				pressedEnter = true;
+			}
+		}
+		#end
+
+		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
+
+		if (gamepad != null)
+		{
+			if (gamepad.justPressed.START)
+				pressedEnter = true;
+
+			#if switch
+			if (gamepad.justPressed.B)
+				pressedEnter = true;
+			#end
+		}
+
+		if (pressedEnter && !transitioning && skippedIntro)
+		{
+			titleText.animation.play('press');
+
+			FlxG.camera.flash(FlxColor.WHITE, 1);
+			FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+
+			transitioning = true;
+			// FlxG.sound.music.stop();
+
+			new FlxTimer().start(2, function(tmr:FlxTimer)
+			{
+				// Check if version is outdated
+
+				var version:String = "v" + Application.current.meta.get('version');
+				/*
+					if (version.trim() != NGio.GAME_VER_NUMS.trim() && !OutdatedSubState.leftState)
+					{
+						FlxG.switchState(new OutdatedSubState());
+						trace('OLD VERSION!');
+						trace('old ver');
+						trace(version.trim());
+						trace('cur ver');
+						trace(NGio.GAME_VER_NUMS.trim());
+					}
+					else
+					{ */
+				Main.switchState(this, new MainMenuState());
+				// }
+			});
+			// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
+		}
+
+		// hi game, please stop crashing its kinda annoyin, thanks!
+		if (pressedEnter && !skippedIntro && initialized)
+		{
+			skipIntro();
+		}
+
+		super.update(elapsed);
+	}
+
+	function createCoolText(textArray:Array<String>)
+	{
+		for (i in 0...textArray.length)
+		{
+			var money:Alphabet = new Alphabet(0, 0, textArray[i], true, false);
+			money.screenCenter(X);
+			money.y += (i * 60) + 200;
+			credGroup.add(money);
+			textGroup.add(money);
+		}
+	}
+
+	function addMoreText(text:String)
+	{
+		var coolText:Alphabet = new Alphabet(0, 0, text, true, false);
+		coolText.screenCenter(X);
+		coolText.y += (textGroup.length * 60) + 200;
+		credGroup.add(coolText);
+		textGroup.add(coolText);
+	}
+
+	function deleteCoolText()
+	{
+		while (textGroup.members.length > 0)
+		{
+			credGroup.remove(textGroup.members[0], true);
+			textGroup.remove(textGroup.members[0], true);
+		}
+	}
+
+	override function beatHit()
+	{
+		super.beatHit();
+
+		logoBl.animation.play('bump');
+		danceLeft = !danceLeft;
+
+		if (danceLeft)
+			gfDance.animation.play('idle');
+		else
+			gfDance.animation.play('idle');
+
+		FlxG.log.add(curBeat);
+
+		switch (curBeat)
+		{
+			case 1:
+				createCoolText(['bam', 'and uh', 'oh its just bam', 'lmao']);
+			case 3:
+				addMoreText('presents');
+			case 4:
+				deleteCoolText();
+			case 5:
+				createCoolText(['Mod of a funky game', 'by']);
+			case 6:
+				addMoreText('ninjamuffin99');
+				addMoreText('PhantomArcade');
+			case 7:
+				addMoreText('kawaisprite\nevilsk8er');
+				addMoreText('evilsk8er');
+			case 8:
+				deleteCoolText();
+				ngSpr.visible = false;
+			case 9:
+				curWacky = FlxG.random.getObject(getIntroTextShit());
+				createCoolText([curWacky[0]]);
+			case 10:
+				addMoreText(curWacky[1]);
+			case 11:
+				deleteCoolText();
+				ngSpr.visible = false;
+			case 13:
+				curWacky = FlxG.random.getObject(getIntroTextShit());
+				createCoolText([curWacky[0]]);
+			case 14:
+				addMoreText(curWacky[1]);
+			case 15:
+				deleteCoolText();
+			case 17:
+				curWacky = FlxG.random.getObject(getIntroTextShit());
+				createCoolText([curWacky[0]]);
+			case 18:
+				addMoreText(curWacky[1]);
+			case 19:
+				deleteCoolText();
+			case 21:
+				curWacky = FlxG.random.getObject(getIntroTextShit());
+				createCoolText([curWacky[0]]);
+			case 22:
+				addMoreText(curWacky[1]);
+			case 23:
+				deleteCoolText();
+			case 25:
+				curWacky = FlxG.random.getObject(getIntroTextShit());
+				createCoolText([curWacky[0]]);
+			case 26:
+				addMoreText(curWacky[1]);
+			case 27:
+				deleteCoolText();
+			// credTextShit.visible = false;
+			// credTextShit.text = "Friday";
+			// credTextShit.screenCenter();
+			case 29:
+				addMoreText('Oh boy');
+			// credTextShit.visible = true;
+			case 30:
+				deleteCoolText();
+				addMoreText('its another');
+			// credTextShit.text += '\nNight';
+			case 31:
+				deleteCoolText();
+				addMoreText('self insert mod'); // credTextShit.text += '\nFunkin';
+
+			case 32:
+				skipIntro();
+		}
+	}
+
+	var skippedIntro:Bool = false;
+
+	function skipIntro():Void
+	{
+		if (!skippedIntro)
+		{
+			remove(ngSpr);
+
+			FlxG.camera.flash(FlxColor.WHITE, 4);
+			remove(credGroup);
+			skippedIntro = true;
+		}
+		//
+	}
+}
